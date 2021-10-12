7638b5aa26b1dc08fc55296eb696a8e407a71824
AlexanderDix <axdxrere@gmail.com>
2021-10-12 11:05:16 +0500

    Добавлены комментарии

--- TelegramBot/Configuration.cs
+++ TelegramBot/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static readonly string BotToken = "2016907380:AAHuxyf6A7MSLOm4hCnhxycmlDDvuzi982s";
+        public static readonly string BotToken = "{TOKEN}";
     }
 }
--- TelegramBot/ListFiles.cs
+++ TelegramBot/ListFiles.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace TelegramBot
 {
+
+    /// <summary>
+    /// Класс для работы со списками, хранящими названия файлов
+    /// </summary>
     public class ListFiles
     {
         public List<string> FileNames { get; set; }
@@ -21,16 +25,28 @@ namespace TelegramBot
             VoiceNames = new List<string>();
         }
 
+        /// <summary>
+        /// Добавление названия файла в список
+        /// </summary>
+        /// <param name="fileName"></param>
         public void AddFileName(string fileName)
         {
             FileNames.Add(fileName);
         }
 
+        /// <summary>
+        /// Добавление названия фотографии в список
+        /// </summary>
+        /// <param name="photoName"></param>
         public void AddPhotoName(string photoName)
         {
             PhotoNames.Add(photoName);
         }
 
+        /// <summary>
+        /// Добавление названия голосовой в список
+        /// </summary>
+        /// <param name="voiceName"></param>
         public void AddVoiceName(string voiceName)
         {
             VoiceNames.Add(voiceName);
