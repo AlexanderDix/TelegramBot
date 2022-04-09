c54ba4370fcb677361e519dc259244345234b4ef
AlexanderDix <axdxrere@gmail.com>
2022-04-09 15:59:53 +0500

    Модель представление

---
+++ TelegramBotUI/Views/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using TelegramBotUI.ViewModels.Base;
+
+namespace TelegramBotUI.Views
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #region Title : string - Заголовок окна
+
+        ///<summary>Заголовок окна</summary>
+        private string _title = "Телеграмм бот UI";
+
+        ///<summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+    }
+}
