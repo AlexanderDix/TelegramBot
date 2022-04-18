4c8ebf185f504302f246c4280c6018c61f504dc5
AlexanderDix <axdxrere@gmail.com>
2022-04-18 13:07:40 +0500

    Реализация UserDialogService для открытия файла, и вывода информационных сообщений

--- TelegramBotUI/Services/Interfaces/IUserDialog.cs
+++ TelegramBotUI/Services/Interfaces/IUserDialog.cs
@@ -2,6 +2,13 @@
 {
     internal interface IUserDialog
     {
-        
+        bool OpenFile(string title, out string selectedFile,
+            string filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*");
+
+        void Information(string title, string message);
+
+        void Warning(string title, string message);
+
+        void Error(string title, string message);
     }
 }
--- TelegramBotUI/Services/UserDialogService.cs
+++ TelegramBotUI/Services/UserDialogService.cs
@@ -1,8 +1,38 @@
-﻿using TelegramBotUI.Services.Interfaces;
+﻿using System.Windows;
+using Microsoft.Win32;
+using TelegramBotUI.Services.Interfaces;
 
 namespace TelegramBotUI.Services
 {
     internal class UserDialogService : IUserDialog
     {
+        public bool OpenFile(string title, out string selectedFile,
+            string filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*")
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                Title = title,
+                Filter = filter
+            };
+
+            if (fileDialog.ShowDialog() != true)
+            {
+                selectedFile = null;
+                return false;
+            }
+
+            selectedFile = fileDialog.FileName;
+
+            return true;
+        }
+
+        public void Information(string title, string message) =>
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+
+        public void Warning(string title, string message) =>
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Warning);
+
+        public void Error(string title, string message) =>
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
     }
 }
